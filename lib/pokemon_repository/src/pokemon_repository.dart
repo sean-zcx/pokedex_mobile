63698d666bfc5f968b17240fb8531e997d08244c
Chenxiang Zhang <zcxsean@gmail.com>
2024-11-30 22:35:23 -0500

    replace with logger

--- lib/pokemon_repository/src/pokemon_repository.dart
+++ lib/pokemon_repository/src/pokemon_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:local_storage/local_storage.dart';
+import 'package:logger/logger.dart';
 import 'package:pokedex_mobile/pokemon_repository/src/models/pokemon_models/pokemon.dart';
 import 'package:pokedex_mobile/pokemon_repository/src/models/pokemon_models/simple_pokemon.dart';
 import 'package:pokemon_api/pokemon_api.dart';
@@ -8,6 +9,7 @@ import 'package:pokemon_api/pokemon_api.dart';
 class PokemonRepository {
   PokemonRepository._();
   static final instance = PokemonRepository._();
+  final logger = Logger();
 
   final PokemonApi _client = PokemonApi.instance;
   bool _initialized = false;
@@ -27,27 +29,27 @@ class PokemonRepository {
   Future<void> _loadSimplePokemonList() async {
     String? data = _localStorage.pokemonStorage.getSimplePokemonList();
     if (data == null) {
-      print('no data');
+      logger.d('no data');
       data = await _client.getSimplePokemopnList();
       _localStorage.pokemonStorage.putSimplePokemonList(data);
     } else {
-      print('has data');
+      logger.d('has data');
     }
 
     final map = jsonDecode(data) as Map<String, dynamic>;
     final list = map['list'] as List<dynamic>;
     simplePokemonList = list.map((e) => SimplePokemon.fromJson(e)).toList();
-    print('length: ${simplePokemonList.length}');
+    logger.d('length: ${simplePokemonList.length}');
   }
 
   Future<Pokemon> getPokemonById(int id) async {
     String? data = _localStorage.pokemonStorage.getPokemon(id);
     if (data == null) {
-      print('no data');
+      logger.d('no data');
       data = await _client.getPokemonById(id);
       _localStorage.pokemonStorage.putPokemon(id, data);
     } else {
-      print('has data');
+      logger.d('has data');
     }
 
     final pokemon = Pokemon.fromJson(jsonDecode(data));
